a037bc8718cac9e04575bac6e2b59e19de0f4deb
LeonTM <lanneken09@gmail.com>
2024-01-23 17:40:52 +0100

    Added server.js

---
+++ src/server.js
@@ -0,0 +1,46 @@
+// Imports
+
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const mongoose = require("mongoose");
+const env = require("dotenv");
+const cors = require("cors");
+
+// Project-Imports
+
+const router = require("./router");
+
+// Presets
+
+const app = express();
+const server = http.createServer(app);
+
+// Configs
+
+env.config();
+
+app.use(compression());
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env["MONGO_URL"]);
+mongoose.connection.on("connected", (stream) =>
+  console.log("Database working properly!")
+);
+mongoose.connection.on("error", (err) => console.log(err));
+
+// Code
+
+server.listen(3000, () => {
+  console.log(
+    "Server running on local port 2000 with url 'http://localhost:2000/'"
+  );
+});
+
+app.use("/", router());
+app.get("/", (req, res) => res.send("Hello World!"));
