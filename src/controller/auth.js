502c8443de8cf0827840b0bcb69209b7ba4f2b69
LeonTM <lanneken09@gmail.com>
2024-01-23 18:11:18 +0100

    Added controller

---
+++ src/controller/auth.js
@@ -0,0 +1,115 @@
+// Imports
+
+const express = require("express");
+
+// Project-Imports
+
+const { createUser, getUserByEmail } = require("../db/users");
+const { authentication, random } = require("../helpers/auth");
+const sendAPIResponse = require("../helpers/sendAPIResponse");
+const validateAccess = require("../helpers/validateAccess");
+
+// Code
+
+const login = async (req, res) => {
+  try {
+    if (!validateAccess(req)) {
+      return res
+        .status(401)
+        .json(sendAPIResponse(401, "Unauthorized.", null, null))
+        .end();
+    }
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json(sendAPIResponse(400, "Check your request once more.", null, null))
+        .end();
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+auth.salt +auth.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.auth.salt, password);
+
+    if (user.auth.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+
+    user.auth.salt = salt;
+
+    await user.save();
+
+    return res.status(200).json(sendAPIResponse(200, "Logged in.", null, null))
+      .end;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(sendAPIResponse(500, "Our bad.", null, null)).end();
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    if (!validateAccess(req)) {
+      return res
+        .status(401)
+        .json(sendAPIResponse(401, "Unauthorized.", null, null))
+        .end();
+    }
+
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      auth: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res
+      .status(201)
+      .json(
+        sendAPIResponse(
+          201,
+          "User was created.",
+          {
+            users: Object.values(user),
+          },
+          "arr"
+        )
+      )
+      .end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(sendAPIResponse(500, "Our fault.", null, null));
+  }
+};
+
+// Exports
+
+module.exports = {
+  login,
+  register,
+};
