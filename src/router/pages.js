39e6bca5c72b9d3f4ef0f438d01a9a57dccc5b52
LeonTM <lanneken09@gmail.com>
2024-01-23 20:54:22 +0100

    Added pages router

---
+++ src/router/pages.js
@@ -0,0 +1,14 @@
+// Imports
+
+const express = require("express");
+
+// Constants
+
+const router = express.Router();
+// Exports
+
+module.exports = () => {
+    router.get("/", (req, res) => {
+        res.render()
+    })
+};
