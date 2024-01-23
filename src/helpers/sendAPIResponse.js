d6cbba645ab6ab1270241fc96323ce1102fd1903
LeonTM <lanneken09@gmail.com>
2024-01-23 18:53:44 +0100

    Updated

--- src/helpers/sendAPIResponse.js
+++ src/helpers/sendAPIResponse.js
@@ -1,41 +1,54 @@
 // Exports
 
+const { ok } = require("assert");
+
 function sendAPIResponse(code, description, contents, responseType) {
-  let message = "";
-  if (code === 200) {
-    message = "OK";
-  } else if (code === 201) {
-    message = "Created";
-  } else if (code === 204) {
-    message = "No Content";
-  } else if (code === 400) {
-    message = "Bad Request";
-  } else if (code === 401) {
-    message = "Unauthorized";
-  } else if (code === 403) {
-    message = "Forbidden";
-  } else if (code === 404) {
-    message = "Not Found";
-  } else if (code === 500) {
-    message = "Internal Server Error";
-  } else if (code === 501) {
-    message = "Not Implemented";
-  } else if (code === 503) {
-    message = "Service Unavailable";
-  } else {
-    message = "Unknown";
-  }
   let response = {
     code: code,
-    message: message,
+    message: "",
     description: description,
     date: new Date(),
-    ok: statusMessages[code].ok,
+    ok: true,
     response: {
       content_type: responseType,
       contents: contents,
     },
   };
+  if (code === 200) {
+    response.message = "OK";
+    response.ok = true;
+  } else if (code === 201) {
+    response.message = "Created";
+    response.ok = true;
+  } else if (code === 204) {
+    response.message = "No Content";
+    response.ok = true;
+  } else if (code === 400) {
+    response.message = "Bad Request";
+    response.ok = false;
+  } else if (code === 401) {
+    response.message = "Unauthorized";
+    response.ok = false;
+  } else if (code === 403) {
+    response.message = "Forbidden";
+    response.ok = false;
+  } else if (code === 404) {
+    response.message = "Not Found";
+    response.ok = false;
+  } else if (code === 500) {
+    response.message = "Internal Server Error";
+    response.ok = false;
+  } else if (code === 501) {
+    response.message = "Not Implemented";
+    response.ok = false;
+  } else if (code === 503) {
+    response.message = "Service Unavailable";
+    response.ok = false;
+  } else {
+    response.message = "Unknown";
+    response.ok = false;
+  }
+
   return response;
 }
 
