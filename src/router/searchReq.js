6f1f841e484e5f75282b63f848b07957af5e994e
LeonTM <lanneken09@gmail.com>
2024-01-24 18:29:23 +0100

    Fixed url

--- src/router/searchReq.js
+++ src/router/searchReq.js
@@ -15,6 +15,6 @@ module.exports = (router) => {
   router.get("/api/searchReqs/get/id=:id/type=:type", getSearchReq);
   router.get("/api/searchReqs/get/creator=:creator", getAllSearchReqsByCreator);
   router.delete("/api/searchReqs/delete/id=:id", deleteSearchReq);
-  router.patch("/searchReqs/update/id=:id/type=:type", updateSearchReq);
+  router.patch("/api/searchReqs/update/id=:id/type=:type", updateSearchReq);
   router.post("/api/searchReqs/create/type=:type", createSearchReq);
 };
