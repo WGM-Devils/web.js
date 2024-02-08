75fb333a15ae30fb4cd85fa322064612247d5e50
LeonTM <lanneken09@gmail.com>
2024-02-08 16:25:36 +0100

    Updated cookie system

--- src/views/public/general/signing/script.js
+++ src/views/public/general/signing/script.js
@@ -29,6 +29,8 @@ function submitCredentials(type) {
       .then((res) => {
         if (res.ok) {
           if (!res.json()) return;
+          const user = res.json().response.contents.users[0];
+          localStorage.setItem("KlingtGut", JSON.stringify(user));
           return (window.location.href = "https://klingt-gut.onrender.com/");
         }
       });
@@ -58,6 +60,8 @@ function submitCredentials(type) {
       })
       .then((res) => {
         if (res.json().ok) {
+          const user = res.json().response.contents.users[0];
+          localStorage.setItem("KlingtGut", JSON.stringify(user));
           return (window.location.href = "https://klingt-gut.onrender.com/");
         }
       });
