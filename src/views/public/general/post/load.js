d11c8be4fd11a6fa3db4aa96694ca919c67ed3a4
LeonTM <lanneken09@gmail.com>
2024-02-09 19:11:16 +0100

    testing further

--- src/views/public/general/post/load.js
+++ src/views/public/general/post/load.js
@@ -43,7 +43,7 @@ async function create(post) {
     })
     .then((data) => {
       if (data.ok) {
-        console.log(data.response.contents.users[0].username);
+        console.log(data.response.contents.users);
         postUserText.innerHTML = "";
       }
     });
