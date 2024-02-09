a0a61c05fe7c1dfa97cc36abc170c4c64e1886de
LeonTM <LAnneken09@gmail.com>
2024-02-09 12:01:18 +0100

    Fixed way of loading account info

--- src/views/public/general/account/script.js
+++ src/views/public/general/account/script.js
@@ -4,10 +4,32 @@ window.onload = () => {
   let accountName = document.getElementById("account-name");
   let accountSubtitle = document.getElementById("account-subtitle");
 
-  if (!localStorage.getItem("KlingtGut")) return;
+  const cookies = document.cookie;
+  if (!cookies.includes("KLINGTGUT.id")) return;
 
-  const user = JSON.parse(localStorage.getItem("KlingtGut"));
-  user.username &&
-    (accountName.innerHTML = user.username) &&
-    (accountSubtitle.innerHTML = "Profil bearbeiten");
+  fetch(
+    `https://klingt-gut.onrender.com/api/user/get/id=${cookies.replace(
+      "KLINGTGUT.id="
+    )}/type=json`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "KlingtGut",
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      if (!data.ok) return;
+
+      const user = data.response.contents;
+      user.username &&
+        (accountName.innerHTML = user.username) &&
+        (accountSubtitle.innerHTML = "Profil bearbeiten");
+    });
 };
